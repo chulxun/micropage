b7fc05d76596cc72407f5da2a46d08c9eb92f778
fanjinyan <fanjinyan@bluecity.com>
2021-10-22 18:49:13 +0800

    解决已有问题

---
+++ vetur.config.js
@@ -0,0 +1,9 @@
+//根目录下配置vetur
+// 这样vue文件内就不会报：Cannot find module '@/xxx/xxx' or its corresponding type declarations.Vetur(2307)
+module.exports = {
+  projects: [
+    {
+      root: './micropage/h5'
+    },
+  ],
+}
