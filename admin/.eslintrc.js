6e453631561ec2104466d0b58bac3d0594483ffe
fanjinyan <fanjinyan@bluecity.com>
2021-12-10 19:58:27 +0800

    feat: 11

--- admin/.eslintrc.js
+++ admin/.eslintrc.js
@@ -1,8 +1,17 @@
 module.exports = {
+  "eslint.workingDirectories": [
+    "src"
+  ],
+  "include": ["**/*.ts", "**/*.tsx", "**/*.js"],
   extends: [require.resolve('@umijs/fabric/dist/eslint')],
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
     page: true,
     REACT_APP_ENV: true,
+  },
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
   },
 };
